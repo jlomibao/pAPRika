f37904650dcc1c3e2a2a035e5ef9f9e64f7306a3
John F Lomibao <jlomibao@entropy.ucsd.edu>
2019-01-25 18:55:07 -0800

    updated test_openmm_amber_restraint.py

--- test/test_openmm_amber_restraint.py
+++ test/test_openmm_amber_restraint.py
@@ -2,6 +2,13 @@ import parmed as pmd
 from paprika import tleap
 from paprika import restraints
 from paprika import amber
+from paprika.openmm import *
+from simtk.openmm.app import *
+from simtk.openmm import *
+import simtk.openmm.openmm as mm
+from simtk.unit import *
+from sys import stdout
+from mdtraj.reporters import NetCDFReporter
 import pytraj as pt
 import numpy as np
 import os
@@ -27,30 +34,49 @@ def setup_restraints():
     traj = pt.load(struct_dir+'/'+coordinate_file,struct_dir+'/'+topology_file)
     guest_init_dist = pt.distance(traj,D[0]+' '+G[0])[0]
 
-    attach_fractions = [0.0,1.0]
+    attach_fractions = [1.0]
     pull_distances = [guest_init_dist,5.0+guest_init_dist,18.0+guest_init_dist]
     windows = [len(attach_fractions), len(pull_distances)]
 
     # Guest Restraints
     guest_distance_rest = [D[0], G[0]]
-    guest_restraint_target = guest_init_dist
-    guest_restraint_target_final = 18.0+guest_init_dist
+    #guest_angle_rest = [D[1], D[0], G[0]]
+    guest_restraint_atoms = [
+        guest_distance_rest,
+        #guest_angle_rest
+    ]
+    guest_restraint_targets = [guest_init_dist]
+    guest_restraint_target_final = [18.0+guest_init_dist]
     guest_restraint_distance_fc = 5.0
+    guest_restraint_angle_fc = 100.0
 
-    rest = restraints.DAT_restraint()
-    rest.continuous_apr = True
-    rest.amber_index = True
-    rest.topology = structure
-    rest.mask1 = guest_distance_rest[0]
-    rest.mask2 = guest_distance_rest[1]
-    rest.attach['fc_final'] = guest_restraint_distance_fc
-    rest.attach['target'] = guest_restraint_target
-    rest.attach['fraction_list'] = attach_fractions
-    rest.pull['target_initial'] = guest_restraint_target
-    rest.pull['target_final'] = guest_restraint_target_final
-    rest.pull['num_windows'] = windows[1]
-    rest.initialize()
-    return rest
+    guest_restraints = []
+    for index, atoms in enumerate(guest_restraint_atoms):
+        if len(atoms) > 2:
+            angle = True
+        else:
+            angle = False
+        this = restraints.DAT_restraint()
+        this.auto_apr = True
+        this.amber_index = True
+        this.topology = structure
+        this.mask1 = atoms[0]
+        this.mask2 = atoms[1]
+        if angle:
+            this.mask3 = atoms[2]
+            this.attach['fc_final'] = guest_restraint_angle_fc
+        else:
+            this.attach['fc_final'] = guest_restraint_distance_fc
+        this.attach['target'] = guest_restraint_targets[index]
+        this.attach['fraction_list'] = attach_fractions
+  
+        this.pull['target_final'] = guest_restraint_target_final[index]
+        this.pull['num_windows'] = windows[1] 
+
+        this.initialize()
+        guest_restraints.append(this)
+
+    return guest_restraints
 
 def create_directory():
     work_dir = 'amber_test'
@@ -59,16 +85,17 @@ def create_directory():
     os.makedirs(work_dir)
     path = './'+work_dir+'/'
 
-    rest = setup_restraints()
-    window_list = restraints.create_window_list([rest])
+    guest_restraints = setup_restraints()
+    window_list = restraints.create_window_list(guest_restraints)
     for window in window_list:
         os.makedirs(path+window)
         
     struct_dir = "cb6-but"
     topology_file = "cb6-but-dum.prmtop"
     coordinate_file = "cb6-but-dum.rst7"
-
-    for window in window_list:
+    
+    pull_distances = [0.0,5.0,18.0]
+    for index, window in enumerate(window_list):
         shutil.copyfile(struct_dir+'/'+topology_file,
                         path+window+'/'+topology_file)
         if window[0] == 'a':
@@ -77,7 +104,7 @@ def create_directory():
         elif window[0] == 'p':
             structure = pmd.load_file(struct_dir+'/'+topology_file,
                                       struct_dir+'/'+coordinate_file)
-            target_difference = rest.phase['pull']['targets'][int(window[1:])]-rest.pull['target_initial']
+            target_difference = pull_distances[index]
             for atom in structure.atoms:
                 if atom.residue.name == 'BUT':
                     atom.xz += target_difference
@@ -85,8 +112,70 @@ def create_directory():
         print(window)
         with open(path+window+'/restraints.in', 'w') as f:
             #problem using amber_restraint_line
-            string = restraints.amber_restraint_line(rest,window)
-            if string is not None:
-                f.write(string)
+            for rest in guest_restraints:
+                string = restraints.amber_restraint_line(rest,window)
+                if string is not None:
+                    f.write(string)
 
-create_directory()
+def amber_minimize_with_restraint():
+    work_dir = 'amber_test'
+    file_prefix = 'cb6-but-dum'
+    path = './'+work_dir+'/'
+    window_list = os.listdir(path)
+    
+    for window in window_list:
+        sim = amber.Simulation()
+        sim.path = path+'/'+window
+        sim.prefix = 'restraint_min'
+        sim.executable = 'sander'
+        sim.inpcrd = file_prefix+'.rst7'
+        sim.ref = file_prefix+'.rst7'
+        sim.topology = file_prefix+'.prmtop'
+        sim.restraint_file = 'restraints.in'
+        sim.config_gb_min()
+        sim.cntrl['ntr'] = 1
+        sim.cntrl['maxcyc'] = 5000
+        sim.cntrl['ncyc'] = 1000
+        sim.cntrl['restraint_wt'] = 50.0
+        sim.cntrl['restraintmask'] = "'@DUM'"
+        sim.run(fail_ok=False)
+
+def openmm_minimize_with_restraint():
+    work_dir = 'amber_test'
+    file_prefix = 'cb6-but-dum'
+    path = './'+work_dir+'/'
+    topology_file = file_prefix+'.prmtop'
+    coordinate_file = file_prefix+'.rst7'
+    window_list = os.listdir(path)
+    
+    for window in window_list: 
+        structure = pmd.load_file(work_dir+'/'+window+'/'+topology_file,
+                                  work_dir+'/'+window+'/'+coordinate_file)
+
+        guest_restraints = setup_restraints()
+        
+        for i, atom in enumerate(structure.atoms):
+            if atom.name == 'DUM':
+                atom.mass = 0.0
+
+        prmtop = AmberPrmtopFile(work_dir+'/'+window+'/'+topology_file)
+        inpcrd = AmberInpcrdFile(work_dir+'/'+window+'/'+coordinate_file)
+        system = prmtop.createSystem(
+            nonbondedMethod=NoCutoff,
+            constraints=HBonds,
+            implicitSolventSaltConc=0.0*moles/liter
+        )
+        integrator = LangevinIntegrator(
+            300*kelvin,
+            1/picosecond,
+            0.002*picoseconds
+        )
+        sim = OpenMM_GB_simulation()
+        OpenMM_GB_simulation.add_openmm_restraints(sim,system,guest_restraints,'pull',int(window[1:]))
+        simulation = Simulation(prmtop.topology, system, integrator, mm.Platform.getPlatformByName('Reference'))
+        simulation.context.setPositions(inpcrd.positions)
+        simulation.minimizeEnergy()
+
+#create_directory()
+#amber_minimize_with_restraint()
+#openmm_minimize_with_restraint()
