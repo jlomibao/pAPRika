3833bc1dcad3a9470070cc8232cc485ea96f4404
John F Lomibao <jlomibao@entropy.ucsd.edu>
2019-01-28 16:42:50 -0800

    added basic test to compare amber/openmm energy results after minimization

--- test/test_openmm_amber_restraint.py
+++ test/test_openmm_amber_restraint.py
@@ -96,6 +96,7 @@ def create_directory():
     
     pull_distances = [0.0,5.0,18.0]
     for index, window in enumerate(window_list):
+        open(path+window+'/no_restraints.in', 'a').close()
         shutil.copyfile(struct_dir+'/'+topology_file,
                         path+window+'/'+topology_file)
         if window[0] == 'a':
@@ -109,7 +110,7 @@ def create_directory():
                 if atom.residue.name == 'BUT':
                     atom.xz += target_difference
             structure.save(path+window+'/'+coordinate_file)
-        print(window)
+
         with open(path+window+'/restraints.in', 'w') as f:
             #problem using amber_restraint_line
             for rest in guest_restraints:
@@ -117,7 +118,7 @@ def create_directory():
                 if string is not None:
                     f.write(string)
 
-def amber_minimize_with_restraint():
+def amber_minimize(hasRestraint=False):
     work_dir = 'amber_test'
     file_prefix = 'cb6-but-dum'
     path = './'+work_dir+'/'
@@ -125,57 +126,113 @@ def amber_minimize_with_restraint():
     
     for window in window_list:
         sim = amber.Simulation()
-        sim.path = path+'/'+window
-        sim.prefix = 'restraint_min'
+        sim.path = path+window
+        if hasRestraint:
+            sim.prefix = 'amber_restraint_min'
+            sim.restraint_file = 'restraints.in'
+        else:
+            sim.prefix = 'amber_min'
+            sim.restraint_file = 'no_restraints.in'
         sim.executable = 'sander'
         sim.inpcrd = file_prefix+'.rst7'
         sim.ref = file_prefix+'.rst7'
         sim.topology = file_prefix+'.prmtop'
-        sim.restraint_file = 'restraints.in'
         sim.config_gb_min()
         sim.cntrl['ntr'] = 1
         sim.cntrl['maxcyc'] = 5000
-        sim.cntrl['ncyc'] = 1000
+        sim.cntrl['ncyc'] = 5000
         sim.cntrl['restraint_wt'] = 50.0
         sim.cntrl['restraintmask'] = "'@DUM'"
         sim.run(fail_ok=False)
 
-def openmm_minimize_with_restraint():
+def openmm_minimize(hasRestraints=False):
     work_dir = 'amber_test'
     file_prefix = 'cb6-but-dum'
     path = './'+work_dir+'/'
     topology_file = file_prefix+'.prmtop'
     coordinate_file = file_prefix+'.rst7'
     window_list = os.listdir(path)
-    
+
+    energy_results = {}    
     for window in window_list: 
         structure = pmd.load_file(work_dir+'/'+window+'/'+topology_file,
                                   work_dir+'/'+window+'/'+coordinate_file)
 
-        guest_restraints = setup_restraints()
-        
         for i, atom in enumerate(structure.atoms):
             if atom.name == 'DUM':
                 atom.mass = 0.0
 
-        prmtop = AmberPrmtopFile(work_dir+'/'+window+'/'+topology_file)
-        inpcrd = AmberInpcrdFile(work_dir+'/'+window+'/'+coordinate_file)
-        system = prmtop.createSystem(
-            nonbondedMethod=NoCutoff,
-            constraints=HBonds,
-            implicitSolventSaltConc=0.0*moles/liter
-        )
-        integrator = LangevinIntegrator(
-            300*kelvin,
-            1/picosecond,
-            0.002*picoseconds
-        )
         sim = OpenMM_GB_simulation()
-        OpenMM_GB_simulation.add_openmm_restraints(sim,system,guest_restraints,'pull',int(window[1:]))
-        simulation = Simulation(prmtop.topology, system, integrator, mm.Platform.getPlatformByName('Reference'))
-        simulation.context.setPositions(inpcrd.positions)
-        simulation.minimizeEnergy()
+        settings = {
+            "platform": "CPU",
+            "coordinates": work_dir+'/'+window+'/'+coordinate_file,
+            "temperature": 300*kelvin,
+            "friction": 1/picosecond,
+            "timestep": 0.002*picoseconds,
+            "nonbonded_method": NoCutoff,
+            "solvent": None,
+            "salt": 0.0*moles/liter,
+            "constraints": HBonds,
+             
+        }
+        #prmtop = AmberPrmtopFile(work_dir+'/'+window+'/'+topology_file)
+        #inpcrd = AmberInpcrdFile(work_dir+'/'+window+'/'+coordinate_file)
+        sim.path = path+window
+        sim.coordinates = work_dir+'/'+window+'/'+coordinate_file
+        sim.topology = work_dir+'/'+window+'/'+topology_file
+        sim.phase = 'pull'
+        sim.window = int(window[1:])
+        system = sim.setup_system(settings, seed=None)
+        if hasRestraints:
+            guest_restraints = setup_restraints()
+            system = sim.add_openmm_restraints(
+                system,guest_restraints,sim.phase,sim.window
+            )
+        simulation = sim.setup_simulation(system,settings)
+        sim.minimize(simulation,save=False)
+        state = simulation.context.getState(getEnergy=True,
+                                            getPositions=True)
+        totalEnergy = state.getPotentialEnergy() + state.getKineticEnergy()
+        totalEnergy = totalEnergy.in_units_of(kilocalories_per_mole)
+        #print(window,"restraints =",hasRestraints,totalEnergy)
+        energy_results[window] = totalEnergy
+    return energy_results
 
+def get_amber_results(hasRestraints=False):
+    energy_results = {}
+    work_dir = 'amber_test'
+    path = './'+work_dir+'/'
+    window_list = os.listdir(path)
+    for window in window_list:
+        if hasRestraints:
+            output_file = "/amber_restraint_min.mdinfo"
+        else:
+            output_file = "/amber_min.mdinfo"
+        with open(path+window+output_file, 'r') as f:
+            filelines = f.readlines()
+            for i,line in enumerate(filelines):
+                if re.search('EAMBER', line):
+                    cols = line.split()
+                    totalEnergy = float(cols[2])*kilocalories_per_mole
+                    energy_results[window] = totalEnergy
+                    #print(window,"restraints =",hasRestraints,totalEnergy)
+    return energy_results
+
+def test_amber_vs_openmm(hasRestraints=False):
+    work_dir = 'amber_test'
+    path = './'+work_dir+'/'
+    window_list = os.listdir(path)
+    amber_energy = get_amber_results(hasRestraints)
+    openmm_energy = openmm_minimize(hasRestraints)
+    for window in window_list:
+        print(window,"restraints =",hasRestraints)
+        print("AMBER:",amber_energy[window])
+        print("OPENMM:",openmm_energy[window])
+        print()
+   
 #create_directory()
-#amber_minimize_with_restraint()
-#openmm_minimize_with_restraint()
+#amber_minimize(hasRestraint=True)
+#amber_minimize(hasRestraint=False)
+
+test_amber_vs_openmm(hasRestraints=False)
+test_amber_vs_openmm(hasRestraints=True)
