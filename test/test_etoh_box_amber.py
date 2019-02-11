0632c7e1c014da4e4022401a13c4f61b38eba719
John F Lomibao <jlomibao@entropy.ucsd.edu>
2019-02-11 14:43:52 -0800

    added etoh_test dir/updated etoh simulation files

--- test/test_etoh_box_amber.py
+++ test/test_etoh_box_amber.py
@@ -9,19 +9,21 @@ def test_amber_etoh_sim():
     sim.config_pbc_md()
     sim.path = path
     sim.restraint_file = 'no_rest.in'
-    sim.executable = 'sander'
+    sim.executable = 'pmemd'
     sim.prefix = 'etoh_amber'
     sim.inpcrd = coordinates
     sim.ref = coordinates
     sim.topology = topology
 
     sim.cntrl['ntb'] = 2
-    sim.cntrl['nstlim'] = 5000
-    sim.cntrl['ntwx'] = 500
+    sim.cntrl['nstlim'] = 10000
+    sim.cntrl['ntwx'] = 250
     sim.cntrl['ntwprt'] = 0
     sim.cntrl['cut'] = 9.0
     sim.cntrl['dt'] = 0.002
 
     sim.run()
+
+
 
 test_amber_etoh_sim()
--- test/test_etoh_box_openmm.py
+++ test/test_etoh_box_openmm.py
@@ -11,12 +11,14 @@ def test_openmm_etoh_sim():
     path = './etoh_test/sim_openmm/'
     topology = 'etoh.prmtop'
     coordinates = 'etoh.rst7'
+    md_out = 'etoh_openmm.csv'
 
     prmtop = app.AmberPrmtopFile(path+topology)
     inpcrd = app.AmberInpcrdFile(path+coordinates)
 
     settings = {
         'nonbonded_method': app.PME,
+        'nonbonded_cutoff': 9*unit.angstrom,
         'constraints': app.HBonds,
         'temperature': 298.15*unit.kelvin,
         'friction': 1/unit.picosecond,
@@ -25,8 +27,11 @@ def test_openmm_etoh_sim():
 
     system = prmtop.createSystem(
         nonbondedMethod = settings['nonbonded_method'],
+        nonbondedCutoff = settings['nonbonded_cutoff'],
         constraints = settings['constraints']
     )
+    barostat = mm.MonteCarloBarostat(1.0*unit.bar,298.15*unit.kelvin,25)
+    system.addForce(barostat)
 
     integrator = LangevinIntegrator(
         settings['temperature'],
@@ -34,21 +39,37 @@ def test_openmm_etoh_sim():
         settings['timestep']
     )
 
-    simulation = app.Simulation(prmtop.topology, system, integrator, mm.Platform.getPlatformByName('Reference'))
+    simulation = app.Simulation(prmtop.topology, system, integrator, mm.Platform.getPlatformByName('CPU'))
     simulation.context.setPositions(inpcrd.positions)
     simulation.context.setPeriodicBoxVectors(*inpcrd.boxVectors)
 
-    simulation.reporters.append(NetCDFReporter('etoh_openmm.nc', 500))
+    simulation.reporters.append(NetCDFReporter(path+'etoh_openmm.nc', 500))
     simulation.reporters.append(
         app.StateDataReporter(
-            stdout, 500,
+            path+md_out, 500,
             step=True,
+            time=True,
             potentialEnergy=True,
+            kineticEnergy=True,
+            totalEnergy=True,
             temperature=True,
             volume=True,
             density=True
         )
     )
-    simulation.step(5000)
+    
+    simulation.step(10000)
+
+def getEnergy(md_out):
+    with open(md_out, 'r') as f:
+        data = [line.strip().split(',') for line in f]
+    for item in data[1:]:
+        energy = float(item[4])*unit.kilojoules_per_mole
+        energy = energy.in_units_of(unit.kilocalories_per_mole)
+        print('Total Energy:', energy)
+
+path = './etoh_test/sim_openmm/'
+md_out = 'etoh_openmm.csv'
 
 test_openmm_etoh_sim()
+getEnergy(path+md_out)
